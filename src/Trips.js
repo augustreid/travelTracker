0f723494d6ab4f3634ddb82cfb2623b32856cdc5
August Reid <c.august.reid@gmail.com>
2021-11-14 14:41:22 -0700

    Add and pass tests for Trips data property and findTripById method

--- src/Trips.js
+++ src/Trips.js
@@ -1,6 +1,12 @@
 class Trips {
   constructor(userTrips) {
+    this.allTrips = userTrips;
+  }
 
+  findTripById(id) {
+    return this.allTrips.find((trip) => {
+      return trip.id === id;
+    })
   }
 }
 
--- test/trips-test.js
+++ test/trips-test.js
@@ -30,6 +30,23 @@ describe('Trips', () => {
     expect(user2Trips).to.be.an.instanceOf(Trips);
   });
 
+  it("should have an array of all trips for a single user", function() {
+    expect(user2Trips.allTrips).to.deep.equal(user2Data);
+  });
+
+  it("should be able to return a single trip with a give id", function() {
+    const result1 = user2Trips.findTripById(100);
+    expect(result1).to.deep.equal({
+      "id": 100,
+      "userID": 2,
+      "destinationID": 6,
+      "travelers": 6,
+      "date": "2020/3/28",
+      "duration": 10,
+      "status": "approved",
+      "suggestedActivities": []
+    })
+  });
 
 
 })
