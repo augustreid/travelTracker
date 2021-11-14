60c2eb5a1b10e5f366a9c78225526e024c3698fa
August Reid <c.august.reid@gmail.com>
2021-11-13 19:19:00 -0700

    Create and pass tests for Destination properties

--- src/Destination.js
+++ src/Destination.js
@@ -1,6 +1,11 @@
 class Destination {
   constructor(place) {
-
+    this.id = place.id;
+    this.destination = place.destination;
+    this.lodgingPerDay = place.estimatedLodgingCostPerDay;
+    this.flightPerPerson = place.estimatedFlightCostPerPerson;
+    this.image = place.image;
+    this.alt = place.alt;
   }
 }
 
--- test/destination-test.js
+++ test/destination-test.js
@@ -23,4 +23,28 @@ describe('Destination', () => {
     expect(destination).to.be.an.instanceOf(Destination);
   })
 
+  it("should have an id that is a number", function() {
+    expect(destination.id).to.equal(6);
+  })
+
+  it("should have a destination", function() {
+    expect(destination.destination).to.equal("Jakarta, Indonesia");
+  })
+
+  it("should have an estimated lodging cost per day", function() {
+    expect(destination.lodgingPerDay).to.equal(70);
+  });
+
+  it("should have an estimated flight cost per person", function() {
+    expect(destination.flightPerPerson).to.equal(890);
+  })
+
+  it("should have an image URL", function() {
+    expect(destination.image).to.equal("https://images.unsplash.com/photo-1555333145-4acf190da336?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80");
+  })
+
+  it("should have alt text for the image", function() {
+    expect(destination.alt).to.equal("lit up city at night");
+  })
+
 });
