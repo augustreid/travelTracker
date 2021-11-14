5b31ae9a9b8249e9005f3896986770b1f5eca33a
August Reid <c.august.reid@gmail.com>
2021-11-13 17:27:21 -0700

    Create and pass test for findTravelerData method

--- src/DataRepo.js
+++ src/DataRepo.js
@@ -2,10 +2,16 @@ class DataRepo {
   constructor(dataSet) {
     this.dataSet = dataSet;
   }
-  
+
   findElementById(id) {
     return this.dataSet.find((element) => {
       return element.id === id;
+    })
+  }
+
+  findTravelerData(travelerID) {
+    return this.dataSet.filter((element) => {
+      return element.userID === travelerID;
     })
   }
 }
--- test/dataRepo-test.js
+++ test/dataRepo-test.js
@@ -41,9 +41,56 @@ describe('DataRepo', () => {
       expect(result2).to.deep.equal(sampleTrips[1]);
     })
 
-    // it("should be able to return an array of a single user's data based on the user ID", function() {
-    //
-    // })
+    it("should be able to return an array of a single user's data based on the user ID", function() {
+      let result3 = tripRepo.findTravelerData(1);
+      let result4 = tripRepo.findTravelerData(2);
+      expect(result3).to.deep.equal(
+      [
+        {
+        "id": 117,
+        "userID": 1,
+        "destinationID": 28,
+        "travelers": 3,
+        "date": "2021/01/09",
+        "duration": 15,
+        "status": "approved",
+        "suggestedActivities": []
+      }
+    ]
+    );
+      expect(result4).to.deep.equal(
+        [
+          {
+          "id": 89,
+          "userID": 2,
+          "destinationID": 10,
+          "travelers": 5,
+          "date": "2019/09/27",
+          "duration": 13,
+          "status": "approved",
+          "suggestedActivities": []
+        }, {
+          "id": 100,
+          "userID": 2,
+          "destinationID": 6,
+          "travelers": 6,
+          "date": "2020/3/28",
+          "duration": 10,
+          "status": "approved",
+          "suggestedActivities": []
+        }, {
+          "id": 116,
+          "userID": 2,
+          "destinationID": 7,
+          "travelers": 3,
+          "date": "2020/04/03",
+          "duration": 8,
+          "status": "approved",
+          "suggestedActivities": []
+        }
+      ]
+    )
+    })
 
 
 });
