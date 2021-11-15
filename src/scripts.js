b2597f823d36b0c00350145651139298097ece00
August Reid <c.august.reid@gmail.com>
2021-11-14 22:13:29 -0700

    Create function to display trip cards

--- src/scripts.js
+++ src/scripts.js
@@ -25,6 +25,7 @@ const renderDashboard = () => {
   displayDashboard();
 }
 
+//render functions
 const renderTravelerData = () => {
   const allTravelers = new DataRepo(sampleTravelers);
   const travelerInfo = allTravelers.findElementById(3);
@@ -35,10 +36,14 @@ const renderTravelerTrips = () => {
   const allTrips = new DataRepo(sampleTrips);
   const travelerTrips = allTrips.findTravelerData(traveler.id);
   userTrips = new Trips(travelerTrips);
+  userTrips.sortTripsByDate();
 }
 
+
+//display functions
 const displayDashboard = () => {
   displayGreeting();
+  displayTrips("past");
 }
 
 const displayGreeting = () => {
@@ -46,7 +51,34 @@ const displayGreeting = () => {
   greeting.innerText = `Welcome back, ${firstName}!`;
 }
 
+const displayTrips = (time) => {
+  const oldTrips = userTrips[time].forEach((trip) => {
+    const vacation = new SingleTrip(trip)
+    pastTrips.innerHTML += `<section class="trip-card">
+      <h3> ${vacation.destination} </h3>
+      <table>
+        <tr>
+          <th> Date: </th>
+          <td> ${vacation.date} </td>
+        </tr>
+        <tr>
+          <th> Days: </th>
+          <td> ${vacation.duration} </td>
+        </tr>
+        <tr>
+          <th> Crew: </th>
+          <td> ${vacation.travelers} </td>
+        </tr>
+      </table>
+    </section>`
+  })
+}
+
+
+//query selectors
 const greeting = document.querySelector("#greeting");
+const pastTrips = document.querySelector("#pastTrips");
+const futureTrips = document.querySelector("#futureTrips");
 
 
 //event listeners
