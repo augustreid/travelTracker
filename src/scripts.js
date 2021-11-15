3122fb217933da6d1cdf76e11d67be207bff7c06
August Reid <c.august.reid@gmail.com>
2021-11-14 21:06:44 -0700

    Import classes into scripts file and begin DOM display with dummy data

--- src/scripts.js
+++ src/scripts.js
@@ -3,9 +3,30 @@
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
+import Trips from '../src/Trips.js';
+import DataRepo from "../src/DataRepo.js";
+import SingleTrip from "../src/SingleTrip.js";
+import Traveler from "../src/Traveler.js";
+import Destination from "../src/Destination.js";
+import {
+  sampleTravelers,
+  samplePlaces,
+  sampleTrips
+} from "../src/dummyData.js";
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
+const traveler;
+const allDestinations;
+const userTrips;
 
-console.log('This is the JavaScript entry file - your code begins here.');
+const renderDashboard = () => {
+  renderTravelerData();
+}
+
+const renderTravelerData = () => {
+  const allTravelers = new DataRepo(sampleTravelers);
+  const travelerInfo = allTravelers.findElementById(3);
+  traveler = new Traveler(travelerInfo);
+}
