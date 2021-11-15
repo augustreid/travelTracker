d14d615259b704948083ea0ce9e297bdcc14b3f3
August Reid <c.august.reid@gmail.com>
2021-11-14 18:06:20 -0700

    Write tests for all trip properties

--- test/SingleTrip-test.js
+++ test/SingleTrip-test.js
@@ -29,6 +29,38 @@ describe('SingleTrip', () => {
 
   it("should be an instance of SingleTrip", function() {
     expect(trip).to.be.an.instanceOf(SingleTrip);
+  });
+
+  it("should have an id", function() {
+    expect(trip.id).to.equal(89);
+  });
+
+  it("should have a userID for the associated traveler", function() {
+    expect(trip.userID).to.equal(2);
+  });
+
+  it("should have a destinationID", function() {
+    expect(trip.destinationID).to.equal(10);
+  });
+
+  it("should have a number of travelers on the trip", function() {
+    expect(trip.travelers).to.equal(5);
+  });
+
+  it("should have a date", function() {
+    expect(trip.date).to.equal("2019/09/27");
+  });
+
+  it("should have a duration that is a number of days", function() {
+    expect(trip.duration).to.equal(13);
+  });
+
+  it("should have a status of approved or pending", function() {
+    expect(trip.status).to.equal("approved");
+  });
+
+  it("should be able to store suggested activities", function() {
+    expect(trip.activities).to.equal([]);
   })
 
 })
